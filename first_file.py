f9bbc43203915a45f2fb5ad19e73a643230e08dc
Sushant Bhattarai <susant@588.local>
2019-05-29 23:57:29 -0700

    Variables

--- first_file.py
+++ first_file.py
@@ -1,2 +1,22 @@
 #Thjs is a new comment
 
+"""how to add, commit and push codes in github.
+#1) git add filename.py
+#2) git commit -m "comment"
+#3) git push
+"""
+
+b = "hello, world!"
+print(b[3])
+
+print(b[0:5])
+
+
+a = " Hello, World   "
+print(a.strip())
+print(len(a))
+
+print(len(b))
+
+print(b.upper())
+
